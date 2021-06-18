3e96e012e7b70151179300a00742610342b7d8a9
gabsgc <gabriellagarccia@gmail.com>
2021-06-17 22:10:21 -0300

    create requirements.txt and up index_controller

--- application/controller/index_controller.py
+++ application/controller/index_controller.py
@@ -1,8 +1,8 @@
-from application.model.entity.medicao import Medicao
-from application.model.dao.medicao_dao import MedicaoDAO
-import datetime
 from application import app
 from flask import jsonify, request
+import datetime
+from application.model.entity.medicao import Medicao
+from application.model.dao.medicao_dao import MedicaoDAO
 
 medicao_list = MedicaoDAO().listarTodos()
 
@@ -16,15 +16,15 @@ def listarMedicao():
 @app.route('/medicao', methods=['POST'])
 def addMedicao():
     id = len(medicao_list) + 1
-    dispositivoId = request.json.get('dispositivoId', None)
-    materia_particulada = float(request.json.get('materiaParticulada', None))
+    dispositivoId = int(request.json.get('dispositivoId', None))
+    materiaParticulada = float(request.json.get('materiaParticulada', None))
     gas = float(request.json.get('gas', None))
     nitrogenio = float(request.json.get('nitrogenio', None))
-    gas_ozonio = float(request.json.get('gasOzonio', None))
+    gasOzonio = float(request.json.get('gasOzonio', None))
     temperatura = float(request.json.get('temperatura', None))
     umidade = float(request.json.get('umidade', None))
-    data = datetime(request.json.get('data', None))
-    medicao = Medicao(id, dispositivoId, materia_particulada, gas, nitrogenio, gas_ozonio, temperatura, umidade, data)
+    data = datetime.strptime(request.json.get('data', None), '%d/%m/%Y' )
+    medicao = Medicao(id, dispositivoId, materiaParticulada, gas, nitrogenio, gasOzonio, temperatura, umidade, data)
     MedicaoDAO().inserir(medicao)
     return medicao.toDict(), 201
 
